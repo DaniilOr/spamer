84a7a2939022e3d9dae9e662ebbd546a89be87a3
DaniilOr <facedan123@gmail.com>
2021-04-24 17:55:48 +0600

    add cors

--- services/backend/cmd/app/server.go
+++ services/backend/cmd/app/server.go
@@ -37,8 +37,17 @@ func NewServer(authSvc *auth.Service, classifierSvc *classifier.Service, spamerS
 
 func (s *Server) Init() error {
 	s.mux.Use(middleware.Logger)
-
 	s.mux.Route("/api", func(r chi.Router) {
+		r.Use(cors.Handler(cors.Options{
+			// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300, // Maximum value not ignored by any of major browsers
+		}))
 		r.Post("/token", s.token)
 		r.With(Auth(func(ctx context.Context, token string) (int64, error) {
 			return s.authSvc.Id(ctx, token)
@@ -47,7 +56,7 @@ func (s *Server) Init() error {
 	s.mux.Route("/classify", func(r chi.Router){
 		r.Post("/url", s.classifyURL)
 		r.Post("/sms", s.classifySMS)
-	})
+	})ss
 
 	return nil
 }
