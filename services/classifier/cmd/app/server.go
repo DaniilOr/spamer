d125ee059aa8186d7bf79f2ad3343acff21fb75d
DaniilOr <facedan123@gmail.com>
2021-04-25 21:27:46 +0600

    TRY redis

--- services/classifier/cmd/app/server.go
+++ services/classifier/cmd/app/server.go
@@ -2,29 +2,51 @@ package app
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/DaniilOr/spamer/services/classifier/pkg/SMSC"
 	"github.com/DaniilOr/spamer/services/classifier/pkg/URLC"
 	serverPb "github.com/DaniilOr/spamer/services/classifier/pkg/server"
+	"github.com/gomodule/redigo/redis"
 	"log"
+	"time"
 )
-
+const cacheTimeout = 50 * time.Millisecond
 type Server struct {
 	Sms *SMSC.Service
 	Url *URLC.Service
 	ctx context.Context
+	cache *redis.Pool
 }
-
-func NewServer(sms *SMSC.Service, url *URLC.Service, ctx context.Context) *Server {
-	return &Server{Sms: sms, Url: url, ctx: ctx}
+type CacheResp struct {
+	Url string `json:"url" bson:"url"`
+}
+func NewServer(sms *SMSC.Service, url *URLC.Service, cache *redis.Pool, ctx context.Context) *Server {
+	return &Server{Sms: sms, Url: url, ctx: ctx, cache: cache}
 }
 
 func (s *Server) CheckURL(ctx context.Context, request *serverPb.URLReq) ( * serverPb.URLResp, error) {
 	log.Println("Enter Check url inside")
+	var r CacheResp
+	if cached, err := s.FromCache(ctx, fmt.Sprintf("urls:%s", request.Url)); err == nil {
+		log.Printf("Got from cache: %s", cached)
+		err = json.Unmarshal(cached, r)
+		if err != nil{
+			return nil, err
+		}
+		return &serverPb.URLResp{Verdict: r.Url}, nil
+	}
 	res, err := s.Url.CheckURL(request.Url)
 	if err != nil {
 		log.Println(err)
 		return nil, err
 	}
+	r.Url = res
+	data, err := json.Marshal(r)
+	if err != nil{
+		return nil, err
+	}
+	err = s.ToCache(ctx, fmt.Sprintf("urls:%s", res), data)
 	response := serverPb.URLResp{Verdict: res}
 	return &response, nil
 }
@@ -37,4 +59,51 @@ func (s *Server) CheckSMS(ctx context.Context, request *serverPb.SMSReq) ( * ser
 	}
 	response := serverPb.SMSResp{Verdict: res}
 	return &response, nil
+}
+
+func (s *Server) FromCache(ctx context.Context, key string) ([]byte, error) {
+	conn, err := s.cache.GetContext(ctx)
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	defer func() {
+		if cerr := conn.Close(); cerr != nil {
+			log.Print(cerr)
+		}
+	}()
+
+	reply, err := redis.DoWithTimeout(conn, cacheTimeout, "GET", key)
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	value, err := redis.Bytes(reply, err)
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+	return value, err
+}
+
+func (s *Server) ToCache(ctx context.Context, key string, value []byte) error {
+	conn, err := s.cache.GetContext(ctx)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	defer func() {
+		if cerr := conn.Close(); cerr != nil {
+			log.Print(cerr)
+		}
+	}()
+
+	_, err = redis.DoWithTimeout(conn, cacheTimeout, "SET", key, value)
+	if err != nil {
+		log.Print(err)
+	}
+	return err
 }
--- services/classifier/cmd/main.go
+++ services/classifier/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/DaniilOr/spamer/services/classifier/pkg/SMSC"
 	"github.com/DaniilOr/spamer/services/classifier/pkg/URLC"
 	serverPb "github.com/DaniilOr/spamer/services/classifier/pkg/server"
+	"github.com/gomodule/redigo/redis"
 	"go.opencensus.io/plugin/ocgrpc"
 	"google.golang.org/grpc"
 	"net"
@@ -17,6 +18,7 @@ const (
 	defaultHost = "0.0.0.0"
 	defaultURL  = "http://flask:5000"
 	defaultSMS = "http://sms_flask:5000"
+	defaultCacheDSN = "redis://localhost:6379/0"
 )
 
 func main() {
@@ -38,12 +40,16 @@ func main() {
 	if !ok{
 		sms = defaultSMS
 	}
-	if err := execute(net.JoinHostPort(host, port), url, sms); err != nil {
+	cacheDSN, ok := os.LookupEnv("APP_CACHE_DSN")
+	if !ok {
+		cacheDSN = defaultCacheDSN
+	}
+	if err := execute(net.JoinHostPort(host, port), url, sms, cacheDSN); err != nil {
 		os.Exit(1)
 	}
 }
 
-func execute(addr string, url string, sms string) error {
+func execute(addr string, url string, sms string, cache string) error {
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		return err
@@ -52,7 +58,13 @@ func execute(addr string, url string, sms string) error {
 	grpcServer := grpc.NewServer(grpc.StatsHandler(&ocgrpc.ServerHandler{}))
 	Smss := SMSC.NewService(sms)
 	Urls := URLC.NewService(url)
-	server := app.NewServer(Smss, Urls, ctx)
+	cacher := &redis.Pool{
+		DialContext: func(ctx context.Context) (redis.Conn, error) {
+			return redis.DialURL(cache)
+		},
+	}
+
+	server := app.NewServer(Smss, Urls, ctx, cache)
 	serverPb.RegisterClassifierServer(grpcServer, server)
 	return grpcServer.Serve(listener)
 }
