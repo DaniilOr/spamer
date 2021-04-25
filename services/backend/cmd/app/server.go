ca5dea3824a7ec7c5d8348d2e3d8b777a1c2b8bc
DaniilOr <facedan123@gmail.com>
2021-04-25 16:50:14 +0600

    fix cors again

--- services/backend/cmd/app/server.go
+++ services/backend/cmd/app/server.go
@@ -57,6 +57,16 @@ func (s *Server) Init() error {
 		})).Post("/spam", s.spam)
 	})
 	s.mux.Route("/classify", func(r chi.Router){
+		r.Use(cors.Handler(cors.Options{
+			// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300, // Maximum value not ignored by any of major browsers
+		}))
 		r.Post("/url", s.classifyURL)
 		r.Post("/sms", s.classifySMS)
 	})
